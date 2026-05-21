f2de5de31a05f55515f1632fcb78bc5887c1e63e
Khyati Desai <khyati.d@kaiinos.com>
2026-05-21 18:02:55 +0530

    Added db migration steps.

---
+++ src/main/resources/db/migration/V2__prefill_basic_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.tasks(completed, title) VALUES (true, 'Task 1');
+INSERT INTO public.tasks(completed, title) VALUES (true, 'Task 2');
+INSERT INTO public.tasks(completed, title) VALUES (false, 'Task 3');
